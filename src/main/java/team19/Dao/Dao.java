a97b27fb1c1b84b1ba52aaa25e5d00fb1b101814
markek1337 <petrovv.mark00@gmail.com>
2022-03-28 11:13:19 +0300

    database conn, security class for login

---
+++ src/main/java/team19/Dao/Dao.java
@@ -0,0 +1,30 @@
+package team19.Dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Dao {
+	
+	private Connection conn;
+	
+	public Dao() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			conn=java.sql.DriverManager.getConnection("jdbc:mysql://localhost:3306/vaalikone", "team19", "kukkuu");
+		} catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void close() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
